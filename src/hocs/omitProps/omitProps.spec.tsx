2429f5bd5abe8ab5e9552c804207fee73df67303
Eric Siebeneich <eric.siebeneich@gmail.com>
2022-12-06 11:34:24 -0600

    Add forward ref to omit props

--- src/hocs/omitProps/omitProps.spec.tsx
+++ src/hocs/omitProps/omitProps.spec.tsx
@@ -1,9 +1,10 @@
 /** @jest-environment jsdom */
 import React from 'react';
 import test from 'jest-gwt';
-import type { FC } from 'react';
+import type { ComponentType } from 'react';
 import { render } from '@testing-library/react';
 
+import type { OmittedPropsComponent } from './omitProps';
 import omitProps from './omitProps';
 
 describe('with providers', () => {
@@ -22,6 +23,21 @@ describe('with providers', () => {
       existing_properties_ARE_rendered,
     },
   });
+
+  test('forwards ref', {
+    given: {
+      properities_to_omit,
+      component_properties,
+      component_WITH_REF,
+    },
+    when: {
+      omitting_properties_WITH_REF,
+      rendering_component_WITH_REF,
+    },
+    then: {
+      component_gets_forwarded_ref,
+    },
+  });
 });
 
 type TProps = {
@@ -33,8 +49,10 @@ type TProps = {
 type Context = {
   omitted: Array<keyof TProps>,
   props: TProps,
-  Component: FC<TProps>,
-  OmittedComponent: FC<TProps>
+  Component: ComponentType<TProps>,
+  RefComponent: OmittedPropsComponent<TProps> & { component_mock: jest.Mock },
+  ref: React.MutableRefObject<any>,
+  OmittedComponent: OmittedPropsComponent<TProps>
 };
 
 function properities_to_omit(this: Context) {
@@ -53,14 +71,34 @@ function component(this: Context) {
   this.Component = jest.fn();
 }
 
+function component_WITH_REF(this: Context) {
+  const component_mock = jest.fn();
+  // eslint-disable-next-line react/display-name -- ref
+  this.RefComponent = React.forwardRef((props, ref) => component_mock(props, ref)) as any;
+  this.RefComponent.component_mock = component_mock;
+
+  console.log(this.RefComponent.component_mock);
+}
+
 function omitting_properties(this: Context) {
   this.OmittedComponent = omitProps(this.omitted, this.Component);
+}
+
+function omitting_properties_WITH_REF(this: Context) {
+  this.OmittedComponent = omitProps(this.omitted, this.RefComponent);
 }
 
 function rendering_component(this: Context) {
   const { OmittedComponent } = this;
 
   render(<OmittedComponent {...this.props} />);
+}
+
+function rendering_component_WITH_REF(this: Context) {
+  const { OmittedComponent } = this;
+  this.ref = React.createRef();
+
+  render(<OmittedComponent {...this.props} ref={this.ref} />);
 }
 
 function omitted_properties_are_NOT_rendered(this: Context) {
@@ -77,5 +115,12 @@ function existing_properties_ARE_rendered(this: Context) {
       three: 'three',
     }),
     expect.anything(),
+  );
+}
+
+function component_gets_forwarded_ref(this: Context) {
+  expect(this.RefComponent.component_mock).toHaveBeenCalledWith(
+    expect.anything(),
+    this.ref,
   );
 }
--- src/hocs/omitProps/omitProps.tsx
+++ src/hocs/omitProps/omitProps.tsx
@@ -1,28 +1,30 @@
 import React, {
   useMemo,
 } from 'react';
-import type {
-  FC,
-} from 'react';
 import { omit } from 'lodash/fp';
+
+export type OmittedPropsComponent<TProps> = React.ForwardRefExoticComponent<
+React.PropsWithoutRef<TProps> & React.RefAttributes<unknown>
+>;
 
 export default <
   TProps extends object
 >(
   propsToRemove: Array<keyof TProps>,
-  Component: FC<TProps>,
-) => {
-  const ExtractedComponent = (props: TProps) => {
+  Component: React.ComponentType<TProps>,
+): OmittedPropsComponent<TProps> => {
+  // eslint-disable-next-line react/display-name -- ref
+  const ExtractedComponent = React.forwardRef((props: TProps, ref) => {
     const remaining = useMemo(
       () => omit(propsToRemove, props),
-      // eslint-disable-next-line react-hooks/exhaustive-deps
+      // eslint-disable-next-line react-hooks/exhaustive-deps -- provided props
       [props, ...propsToRemove],
     );
 
     return (
-      <Component {...remaining as any} />
+      <Component {...remaining as any} ref={ref} />
     );
-  };
+  });
 
   return ExtractedComponent;
 };
